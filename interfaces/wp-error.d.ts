6b46f206b65b03fbb01a3b8f474a48b9c29f88dc
John Blackbourn <johnbillion@gmail.com>
2020-05-17 00:05:47 +0100

    Docs for the error class.

--- interfaces/wp-error.d.ts
+++ interfaces/wp-error.d.ts
@@ -1,4 +1,19 @@
+/**
+ * WordPress Error class.
+ *
+ * Container for checking for WordPress errors and error messages. Many
+ * core WordPress functions pass this class in the event of an error.
+ */
 export interface WP_Error {
+
+  /**
+   * Stores the list of errors.
+   */
   errors: any;
+
+  /**
+   * Stores the list of data for error codes.
+   */
   error_data: any;
+
 }
