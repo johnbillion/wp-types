9cbfa9072664884151cf0942800b4012a0d091c4
John Blackbourn <johnbillion@gmail.com>
2020-05-17 01:09:42 +0100

    Introduce a bunch more typings for error objects.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 // Interfaces:
-export { WP_Error } from './interfaces/wp-error';
-export { WP_Post } from './interfaces/wp-post';
+export * from './interfaces/wp-error';
+export * from './interfaces/wp-post';
 
 // Types:
-export { WP_Post_Status_Name } from './types/wp-post-status-name';
-export { WP_Post_Type_Name } from './types/wp-post-type-name';
+export * from './types/wp-post-status-name';
+export * from './types/wp-post-type-name';
